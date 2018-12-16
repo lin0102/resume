f31a466f2fcbf88e85bc853a210fbf569c17a6fd
coderming <coderming@foxmail.com>
2018-12-17 00:01:01 +0800

    实锤了就不怕添加实习信息了～

--- src/config.js
+++ src/config.js
@@ -126,6 +126,17 @@ const config = {
          * 由于HTML的解析特性，如果想同时显示多个空格，请使用&nbsp
          */
         {
+          left: '腾讯（深圳）PCG · QQ支付联合产品部',
+          right: '2019.1 至今',
+          showDot: false,
+          bold: true
+        },
+        {
+          left: '<b>前端实习生</small></b> ...',
+          right: '',
+          showDot: true
+        },
+        {
           left: '重庆邮电大学 · 红岩网校工作站',
           right: '2017.6 至今',
           showDot: false,
